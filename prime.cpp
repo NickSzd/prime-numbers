81b4e3437ae3b3809f25880136884cf997585068
NickSzd <nsszwed@gmail.com>
2021-09-08 15:24:22 -0700

    Removed Random Comments

--- prime.cpp
+++ prime.cpp
@@ -5,21 +5,33 @@
 
 using namespace std;
 
-//cd Temp/C_C++/C++ Projects/Prime-Numbers/
+
 
 void primes(int r)
 {
   vector<int> primes;
   vector<int> nums;
 
-  int n = 200;
+  //int n = 200;
+  //Stes all the initial values of the vector equal to true(1)
   for(int i = 0; i <= r; i++)
   {
     nums.push_back(1);
   }
+  //Prime numbers are all greater than 1
   nums[0] = 0;
   nums[1] = 0;
 
+  /*
+  for i = 2 and i <= range the user wants to know prime number
+    check if nums[i] is 1
+      if a 1 is present
+        enter the 2nd for loop which will make all multiples of that prime number 0
+        this will eliminate many of the non prime numbers
+      else
+        itterate
+
+  */
   for(int i = 2; i <= r; i++)
   {
     if(nums[i] == 1)
@@ -31,6 +43,8 @@ void primes(int r)
     }
   }
 
+  //Takes the prime numbers and puts them in the correct std::vector
+  //May just chhange the function to return the vector
   for(int i = 0; i < nums.size(); i++)
   {
     nums[i] = nums[i] * i;
@@ -44,65 +58,16 @@ void primes(int r)
     }
   }
 
+  //Displays a list of prime numbers
   for(int i = 0; i < primes.size(); i++)
   {
     cout<<primes[i]<<" ";
-    if(i%25 == 0)
-      cout<<'\n';
   }
 
 }
 
 int main(void)
 {
-
+  //Example Input
   primes(1000);
-
-//true means the num != 0
-/*
-  vector<int> primes;
-  vector<int> nums;
-
-  int n = 200;
-  for(int i = 0; i <= n; i++)
-  {
-    nums.push_back(1);
-  }
-  nums[0] = 0;
-  nums[1] = 0;
-
-
-  //itn temp = 0;
-  for(int i = 2; i <= n; i++)
-  {
-    if(nums[i] == 1)
-    {
-      for(int j = 2; i*j <= n; j++)
-      {
-        nums[i*j] = 0;
-      }
-    }
-  }
-
-  for(int i = 0; i < nums.size(); i++)
-  {
-    nums[i] = nums[i] * i;
-    if(nums[i] == 0)
-    {
-      ;
-    }
-    else
-    {
-      primes.push_back(nums[i]);
-    }
-  }
-
-  for(int i = 0; i < primes.size(); i++)
-    cout<<primes[i]<<" ";
-
-*/
-
-
-
-
 }
